44c961f1a537d15ae666a9712f814c4ee615f6b3
spardesh <143691488+spardesh@users.noreply.github.com>
2023-09-16 10:47:24 -0600

    Update shipment_pairs.py

--- src/shipment_pairs.py
+++ src/shipment_pairs.py
@@ -29,12 +29,17 @@ SHIPMENTS = [
     ['VE', 'VE', 13],
  ]
 
-
 def remove_intra(shipments):
     """remove_intra removes any records in which the origin and destination are the
     same country.
+
+    Args:
+        shipments (list): List of shipments with format [ORIGIN, DESTINATION, QUANTITY].
+
+    Returns:
+        list: List of shipments with intra-country shipments removed.
     """
-    pass
+    return [shipment for shipment in shipments if shipment[0] != shipment[1]]
 
 
 def collect_pairs(shipments):
@@ -45,23 +50,28 @@ def collect_pairs(shipments):
     be:
         COUNTRY_A, COUNTRY_B, QUANTITY_AB, QUANTITY_BA
 
-    Example:
-    >>> shipments = [
-         ['US', 'IN', 14],
-         ['US', 'CA', 17],
-         ['IN', 'US', 8],
-         ['IN', 'CA', 12],
-         ['CA', 'US', 7],
-         ['CA', 'IN', 5],
-     ]
-    >>> collect_pairs(shipments)
-    [
-        ['US', 'CA', 17, 7],
-        ['US', 'IN', 14, 8],
-        ['IN', 'CA', 12, 5],
-    ]
+    Args:
+        shipments (list): List of shipments with format [ORIGIN, DESTINATION, QUANTITY].
+
+    Returns:
+        list: List of collected pairs with format [COUNTRY_A, COUNTRY_B, QUANTITY_AB, QUANTITY_BA].
     """
-    pass
+    shipment_pairs = {}
+    
+    for shipment in shipments:
+        origin, destination, quantity = shipment
+        pair = (origin, destination)
+        
+        if pair not in shipment_pairs and (destination, origin) not in shipment_pairs:
+            shipment_pairs[pair] = [quantity, 0]
+        elif pair in shipment_pairs:
+            shipment_pairs[pair][0] += quantity
+        else:
+            shipment_pairs[(destination, origin)][1] += quantity
+    
+    sorted_pairs = sorted(shipment_pairs.items(), key=lambda x: x[1][0], reverse=True)
+    
+    return [[pair[0][0], pair[0][1], pair[1][0], pair[1][1]] for pair in sorted_pairs]
 
 
 if __name__ == '__main__':
@@ -75,5 +85,3 @@ if __name__ == '__main__':
     shipment_pairs = collect_pairs(SHIPMENTS)
     print('\n', '#' * 80, ' ' * 30 + 'Pairs collected', '#' * 80, sep='\n')
     print(shipment_pairs)
-
-
